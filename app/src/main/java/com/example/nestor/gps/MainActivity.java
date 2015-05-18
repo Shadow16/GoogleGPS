382a22bddd1372fb12c3bdf69ee55a0502f31358
Nestor Eduardo <nestor_infante@hotmail.com>
2015-05-17 19:25:05 -0500

    COMMIT

---
+++ app/src/main/java/com/example/nestor/gps/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.nestor.gps;
+
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.jonathan.gps.R;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final LocationListener mLocationListener =  new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                TextView txtlatitud = (TextView) findViewById(R.id.txtlatitud);
+                TextView txtLongitud = (TextView) findViewById(R.id.txtlongitud);
+
+                txtlatitud.setText(Double.toString(location.getLatitude()));
+                txtLongitud.setText(Double.toString(location.getLongitude()));
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        LocationManager mLocationManager =  (LocationManager) getSystemService(LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, mLocationListener);
+    }
+
+    public void cargarMapa(View view){
+
+        TextView txtlatitud = (TextView) findViewById(R.id.txtlatitud);
+        TextView txtLongitud = (TextView) findViewById(R.id.txtlongitud);
+
+        String latitud = txtlatitud.getText().toString();
+        String longitud = txtLongitud.getText().toString();
+
+        WebView webView = (WebView) findViewById(R.id.webVMaps);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com/maps?q=" + latitud + "," + longitud);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
